844d0b825687a2361798348d3baca3595f7248aa
Andrey <boombardir20@gmail.com>
2023-01-07 18:27:08 +0500

    currentQuestionIndex no change html

--- QuizApp/src/components/QuizHeader.vue
+++ QuizApp/src/components/QuizHeader.vue
@@ -1,10 +1,14 @@
 <script setup>
 
+const props = defineProps({
+  questionStatus: String,
+})
+
 </script>
 
 <template>
   <header>
-    <h4>Question 1/3</h4>
+    <h4>Question {{ questionStatus }}</h4>
     <div class="bar">
       <div class="completion"></div>
     </div>
--- QuizApp/src/views/QuizView.vue
+++ QuizApp/src/views/QuizView.vue
@@ -2,25 +2,28 @@
 import Question from "@/components/Question.vue";
 import QuizHeader from "@/components/QuizHeader.vue";
 import {useRoute} from "vue-router";
-import {ref} from "vue";
+import {ref, watch, computed} from "vue";
 import quizes from "@/data/quizes.json";
 
 const route = useRoute();
-
 const quizId = parseInt(route.params.id);
-
 const quiz = quizes.find((quiz) => quiz.id === quizId);
-
 const currentQuestionIndex = ref(0); //"questions" array index, from json file
+
+const questionStatus = computed(() => {
+  return `${currentQuestionIndex.value}/${quiz.questions.length}`;
+});
+
 
 </script>
 
 <template>
   <div>
-    <QuizHeader/>
+    <QuizHeader :questionStatus="questionStatus"/>
     <div>
       <Question :question="quiz.questions[currentQuestionIndex]"/>
     </div>
+    <button @click="currentQuestionIndex++">Next Question</button>
   </div>
 </template>
 
