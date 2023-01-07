c473976ecb003ad3ba56913730d61f84f58e13f3
Andrey <boombardir20@gmail.com>
2023-01-07 20:07:51 +0500

    Result.vue

---
+++ QuizApp/src/components/Result.vue
@@ -0,0 +1,45 @@
+<script setup>
+import {RouterLink} from "vue-router";
+
+const {
+  quizQuestionsLength,
+  numberOfCorrectAnswers
+} = defineProps(["quizQuestionsLength", "numberOfCorrectAnswers"]);
+
+</script>
+
+<template>
+  <div class="results">
+    <p>Results...</p>
+    <h1>You got {{ numberOfCorrectAnswers }} / {{ quizQuestionsLength }} correct</h1>
+    <RouterLink class="restart" to="/">Restart</RouterLink>
+  </div>
+</template>
+
+
+<style scoped>
+.results {
+  text-align: center;
+  padding: 100px 0;
+}
+
+p {
+  font-size: 30px;
+}
+
+h1 {
+  font-size: 100px;
+  color: #777;
+}
+
+.restart {
+  margin-top: 20px;
+  font-size: 30px;
+  color: black;
+  text-decoration: none;
+}
+
+.restart:hover {
+  text-decoration: underline;
+}
+</style>
--- QuizApp/src/views/QuizView.vue
+++ QuizApp/src/views/QuizView.vue
@@ -1,15 +1,17 @@
 <script setup>
 import Question from "@/components/Question.vue";
 import QuizHeader from "@/components/QuizHeader.vue";
+import Result from "@/components/Result.vue";
 import {useRoute} from "vue-router";
 import {ref, watch, computed} from "vue";
-import q from "@/data/quizes.json";
+import quizes from "@/data/quizes.json";
 
 const route = useRoute();
 const quizId = parseInt(route.params.id);
-const quiz = q.find((q) => q.id === quizId);
+const quiz = quizes.find((quiz) => quiz.id === quizId);
 const currentQuestionIndex = ref(0); //"questions" array index, from json file
 const numberOfCorrectAnswers = ref(0);
+const showResult = ref(false);
 
 const questionStatus = computed(() => {
   return `${currentQuestionIndex.value}/${quiz.questions.length}`;
@@ -19,15 +21,18 @@ const barWidth = computed(() => {
 });
 
 const nextQuestion = () => {
-  // if (currentQuestionIndex.value < quiz.questions.length) {
   currentQuestionIndex.value++;
-  // }
 };
 
 const onOptionSelected = (isCorrect) => {
   if (isCorrect) {
     numberOfCorrectAnswers.value++;
   }
+
+  if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+    showResult.value = true;
+  }
+
   nextQuestion();
 };
 
@@ -37,9 +42,9 @@ const onOptionSelected = (isCorrect) => {
   <div>
     <QuizHeader :questionStatus="questionStatus" :barWidth="barWidth"/>
     <div>
-      <Question :question="quiz.questions[currentQuestionIndex]" @selectOption="onOptionSelected"/>
+      <Question v-if="!showResult" :question="quiz.questions[currentQuestionIndex]" @selectOption="onOptionSelected"/>
+      <Result :quizQuestionsLength="quiz.questions.length" :numberOfCorrectAnswers="numberOfCorrectAnswers" v-else/>
     </div>
-    <button @click="nextQuestion">Next Question</button>
   </div>
 </template>
 
